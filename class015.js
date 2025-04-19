3f46b8cf578137fedabab3080fc83ba57a83694e
Shaniza <shaniza.rr17@gmail.com>
2025-04-20 01:05:05 +0600

    class015 JS operators

---
+++ class015.js
@@ -0,0 +1,23 @@
+//arithmetic operator skip
+
+// conditional operator 
+var a = 10
+var b = 20
+console.log(a == b)//false
+console.log(a != b)//true
+
+console.log(a > b)//false
+console.log(a < b)//true
+
+console.log(a >= b)//false
+console.log(a <= b)//true
+console.log(a === b)
+
+//logical operator
+// && || !
+
+//bitwise operator
+
+//typeof operator
+console.log("typeof 10 - " + typeof 10)
+console.log("typeof 'we' - " + typeof "we")
