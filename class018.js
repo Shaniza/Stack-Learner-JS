561bd5ad6d1e81eca0afc2db22e587386a092711
Shaniza <shaniza.rr17@gmail.com>
2025-04-20 01:22:07 +0600

    class018 js date

---
+++ class018.js
@@ -0,0 +1,15 @@
+var date = new Date()
+console.log(date)
+console.log(date.getDate())
+console.log(date.getMonth())
+console.log(date.getFullYear())
+console.log(date.getDay())
+console.log(date.getTime())//1 Jan 1970 থেকে এই মুহূর্ত পর্যন্ত কত মিলিসেকেন্ড পার হয়েছে
+console.log(date.getHours())
+console.log(date.getMinutes())
+
+console.log(date.getTimezoneOffset())
+console.log(date.toDateString())
+console.log(date.toTimeString())
+console.log(date.toLocaleString())
+console.log(Date.now()); //1 Jan 1970 থেকে এই মুহূর্ত পর্যন্ত কত মিলিসেকেন্ড পার হয়েছে
