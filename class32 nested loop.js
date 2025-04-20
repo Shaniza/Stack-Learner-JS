35590146e3dbbc4d1db592f511a270694bc8c1bc
Shaniza <shaniza.rr17@gmail.com>
2025-04-20 18:15:20 +0600

    class32 nested loop

---
+++ class32 nested loop.js
@@ -0,0 +1,19 @@
+var i = 0
+var j = 0
+for(i=1; i<=5; i++){
+    var result =''
+    for(j=1; j<=i; j++){
+        result = result + j + ' '
+    }
+    console.log(result)
+}
+var n = 10
+for(var p = 0; p < n ; p++){
+    var square = ''
+    for(var q = 0; q < n; q++){
+        square = square + "* "
+
+    }
+    console.log(square)
+ 
+}
