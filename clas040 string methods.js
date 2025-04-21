fd5dbd82c8f39cdbff07aa8cb3b36337e01dab02
Shaniza <shaniza.rr17@gmail.com>
2025-04-21 11:55:00 +0600

    JS string methods

---
+++ clas040 string methods.js
@@ -0,0 +1,17 @@
+var a= "I am"
+var b = "a cat"
+console.log(a.toUpperCase()) 
+console.log(b.toLowerCase())
+var c = a.concat(' ',b)
+console.log(c)
+
+var d = c.substring(2)
+console.log(d)
+
+console.log(c.startsWith("I am"))
+console.log(c.endsWith("cat"))
+console.log(c.endsWith("c"))
+console.log(' enfinia       '.trim())
+console.log(' en    finia       '.trim())
+console.log(c.split(' '))
+console.log(c.split('a'))
