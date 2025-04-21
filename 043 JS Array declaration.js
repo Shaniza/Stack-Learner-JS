b22d719e22e1ccc20f002cbd7199c222d886171c
Shaniza <shaniza.rr17@gmail.com>
2025-04-21 20:06:56 +0600

    JS Array declaration

---
+++ 043 JS Array declaration.js
@@ -0,0 +1,12 @@
+var arr = [1, 2, 3, 4, 5]
+arr[3] = 22
+console.log(arr)
+arr[10] = 33
+console.log(arr)
+arr[6] = 33
+console.log(arr)
+console.log(arr[5])
+console.log(arr.length)
+
+var arr2 = Array(5)
+console.log(arr2)
