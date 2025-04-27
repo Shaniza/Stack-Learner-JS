cf36963f8751d8cd1b72c99ad9e3094656158c22
Shaniza <shaniza.rr17@gmail.com>
2025-04-27 23:47:39 +0600

    Foreach Implementation in Javascript

---
+++ 073 Foreach Implementation in Javascript.js
@@ -0,0 +1,23 @@
+var arr = [1, 2, 3, 4, 5]
+
+var sum = 0
+arr.forEach(function (value, index, arr){
+    console.log(value, index, arr)
+     
+})
+
+// console.log(sum)
+//ei kaj ta manually kora
+var arr1 = [1, 2, 3, 4, 5]
+
+function forEach(arr1, cb){
+    for(var i = 0; i < arr1.length ; i++){
+        cb(arr[i], i, arr1)
+
+    }
+}
+
+forEach(arr, function(value, index, arr1){
+    console.log(value, index, arr1)
+    
+})
