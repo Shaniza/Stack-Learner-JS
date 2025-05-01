15a4ddac170a3239e4212e703f5f92dd21dd9743
Shaniza <shaniza.rr17@gmail.com>
2025-05-01 23:47:55 +0600

    Function Declaration vs Expression in Javascript

---
+++ 090 Function Declaration vs Expression in Javascript.js
@@ -0,0 +1,14 @@
+//function declaration 
+abc()
+
+function abc(){
+    console.log("I am a function declaration")
+}
+
+
+//function expression
+//when function is kept in a variable
+newABC()
+var newABC = function(){
+    console.log("I am function expression")
+}
